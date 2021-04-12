5cb14bf70d9763dc4cd5e2edad845988cb2e8b3e
jesusmaing <martinxf22@gmail.com>
2021-04-11 19:20:37 -0700

    hjkg

--- UNISON/TEST.py
+++ UNISON/TEST.py
@@ -4,5 +4,4 @@ print("Hola sebas");
 print("hola mis amores")
 print("Tu hermana en tanga pinche paramo")
 print("Chinga tu madre pinche paramo jijuelaverga")
-for i in range (10):
-    print(i)
+print("Cuelaverga")
